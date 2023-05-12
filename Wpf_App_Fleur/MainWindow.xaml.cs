a3777d567ead752f933ad4c151bc4af4333b63d4
Paul BOURCEREAU <paul.bourcereau@edu.devinci.fr>
2023-05-12 23:18:29 +0200

    Update main

--- Wpf_App_Fleur/MainWindow.xaml.cs
+++ Wpf_App_Fleur/MainWindow.xaml.cs
@@ -29,112 +29,37 @@ namespace Wpf_App_Fleur
         public MainWindow()
         {
             InitializeComponent();
-
-            /*
-            string videoPath = "C:/Users/TRAVAIL/Desktop/BDDI/Wpf_App_Fleur/video_fleur4.mp4";
-            videoPlayer.Source = new Uri(videoPath, UriKind.RelativeOrAbsolute);
-            videoPlayer.LoadedBehavior = MediaState.Manual;
-            videoPlayer.MediaEnded += video_media_ended;
-            */
         }
-        /*
-        private void video_media_ended(object sender, RoutedEventArgs e)
-        {
-            videoPlayer.Position = TimeSpan.Zero;
-            videoPlayer.Play();
-        }*/
         private void BtnConnectionClick(object sender, RoutedEventArgs e)
         {
             // Informations de connexion à la base de données
             string port = "3306";
             string server = "localhost";
-            string database = "fleur";
+            string database = "fleurs";
 
             string id_admin = "root";
             string password_admin = "root";
 
-            string id_utili = "bozo";
-            string password_utili = "bozo";
             // Chaînes de connexion pour se connecter à la base de données
             string connectionString_admin = $"server={server};port={port};database={database};uid={id_admin};password={password_admin}";
-            string connectionString_utili = $"server={server};port={port};database={database};uid={id_utili};password={password_utili}";
 
-            MySqlConnection connection_admin = new MySqlConnection(connectionString_admin);
-            MySqlConnection connection_utili = new MySqlConnection(connectionString_utili);
-
-            /*string exportPath = @"C:\Users\TRAVAIL\Desktop\BDDI\Fleur_BDDI\";
-            string exportXml = "export_xml.xml";
-            string exportJson = "export_json.js";
-            if (!Directory.Exists(exportPath))
-            {
-                Console.WriteLine("File path does not match!");
-                System.Environment.Exit(1);
-            }*/
 
             //Recuperation de l'identifiant et du mot de passe sasis
-            string username = txtUsername.Text; //"bozo";
-            string password = txtPassword.Password; //"bozo";
+            string username = txtUsername.Text;
+            string password = txtPassword.Password;
 
-            if(username == "root" && password=="root")
+            if(username == id_admin && password==password_admin)
             {
+                MySqlConnection connection_admin = new MySqlConnection(connectionString_admin);
                 connection_admin.Open();
                 AdminWindow admin_window = new AdminWindow(connection_admin);
                 Window.GetWindow(this).Content = admin_window;
-                //Close();
-            }
-            else if (username == "bozo" && password=="bozo")
+            } else
             {
-                BozoWindow bozo_window = new BozoWindow(connection_utili);
-                Window.GetWindow(this).Content = bozo_window;
-                //Close();
-            }
-            else
-            {
-                connection_utili.Open();
-                MySqlCommand command = connection_utili.CreateCommand();
-                command.CommandText = $"select count(*) from client where mail = @username and mdp = @password;";
-                command.Parameters.AddWithValue("@username", username);
-                command.Parameters.AddWithValue("@password", password);
-                using (MySqlDataReader reader = command.ExecuteReader())
-                {
-                    reader.Read();
-                    int count = Convert.ToInt32(reader.GetValue(0));
-                    if (count>0)
-                    {
-                        ClientWindow client_window = new ClientWindow(connection_utili);
-                        Window.GetWindow(this).Content = client_window;
-                        //Close();
-                    }
-                    else
-                    {
-                        MessageBox.Show(count.ToString());
-                        //IsReadOnly = false;
-                    }
-                }
-                connection_utili.Close();
-                command.Dispose();
+                MessageBox.Show("Identifiants érronés");
             }
         }
-        /*
-        private bool isReadOnly;
-        private bool IsReadOnly
-        {
-            get { return isReadOnly; }
-            set
-            {
-                isReadOnly = value;
-                OnPropertyChanged(nameof(IsReadOnly));
-            }
-        }
-        */
-        private void show_password_checked(object sender, RoutedEventArgs e)
-        {
-            //txtPassword.PasswordChar = '\0';
-        }
-        private void show_password_unchecked(object sender, RoutedEventArgs e)
-        {
-            //txtPassword.PasswordChar = '*';
-        }
+
         private void BtnQuitterClick(object sender, RoutedEventArgs e)
         {
             Window.GetWindow(this).Close();
